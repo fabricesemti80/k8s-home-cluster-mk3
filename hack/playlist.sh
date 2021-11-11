e71510671b9b3e6ea9e9ae47b0bd91bb964d4bce
fabricesemti80 <emilfabrice@gmail.com>
2021-11-11 15:26:57 +0000

    pre commit update

--- hack/playlist.sh
+++ hack/playlist.sh
@@ -7,10 +7,13 @@
 while true; do
     figlet -f slant "default" | lolcat
     kubecolor get po -n default; sleep 5; clear;
+
     figlet -f slant "media" | lolcat
     kubecolor get po -n media; sleep 5; clear;
+
     figlet -f slant "home" | lolcat
     kubecolor get po -n home; sleep 5; clear;
+
     figlet -f slant "nodes" | lolcat
     kubecolor get nodes; sleep 5; clear;
 done
